40aa4ffbec7acfb1ef9e7294599afe65b946d4f0
wytsai7660 <wytsai7660@gmail.com>
2023-09-13 14:42:33 +0800

    -

---
+++ project_1/maze.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// #include "maze_generator.c"
+
+// void test(int **mat, unsigned n) {
+//   for (unsigned i = 0; i < n; i++)
+//     for (unsigned j = 0; j < n; j++) mat[i][j] = 99;
+// }
+
+int main() {
+  //   const unsigned n = 10;  // row & col
+  //   int *ptr = malloc(sizeof(int));
+  //   *ptr = 99;
+  //   ptr = malloc(sizeof(int));
+  //   printf("%d", *ptr);
+  printf("hello there!\n");
+  //   scanf("%u", &n);
+
+  //   int **maze = malloc(n * sizeof(int *));
+  //   for (unsigned row = 0; row < n; row++) maze[row] = malloc(n * sizeof(int));
+  //   for (unsigned i = 0; i < n; i++)
+  //     for (unsigned j = 0; j < n; j++) maze[i][j] = 10;
+
+  //   //   test(maze, n);
+  //   for (unsigned i = 0; i < n; i++) {
+  //     for (unsigned j = 0; j < n; j++) printf("%d ", maze[i][j]);
+  //     printf("\n");
+  //   }
+}
