5d173ca1b3544c6db7cb77d766a551cd3efeb908
wytsai7660 <wytsai7660@gmail.com>
2023-09-13 06:59:38 +0000

    -

--- project_1/maze.c
+++ project_1/maze.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// #include "maze_generator.c"
+#include "maze_generator.c"
 
-// void test(int **mat, unsigned n) {
-//   for (unsigned i = 0; i < n; i++)
-//     for (unsigned j = 0; j < n; j++) mat[i][j] = 99;
-// }
+void test(int **mat, unsigned n) {
+  for (unsigned i = 0; i < n; i++)
+    for (unsigned j = 0; j < n; j++) mat[i][j] = 99;
+}
+
+void print(int **mat, unsigned n) {
+  for (unsigned i = 0; i < n; i++) {
+    for (unsigned j = 0; j < n; j++) printf("%d ", mat[i][j]);
+    printf("\n");
+  }
+}
 
 int main() {
-  //   const unsigned n = 10;  // row & col
-  //   int *ptr = malloc(sizeof(int));
-  //   *ptr = 99;
-  //   ptr = malloc(sizeof(int));
-  //   printf("%d", *ptr);
-  printf("hello there!\n");
-  //   scanf("%u", &n);
+  const unsigned n = 10;  // row & col
 
-  //   int **maze = malloc(n * sizeof(int *));
-  //   for (unsigned row = 0; row < n; row++) maze[row] = malloc(n * sizeof(int));
-  //   for (unsigned i = 0; i < n; i++)
-  //     for (unsigned j = 0; j < n; j++) maze[i][j] = 10;
+  int **maze = malloc(n * sizeof(int *));
+  for (unsigned row = 0; row < n; row++) maze[row] = malloc(n * sizeof(int));
 
-  //   //   test(maze, n);
-  //   for (unsigned i = 0; i < n; i++) {
-  //     for (unsigned j = 0; j < n; j++) printf("%d ", maze[i][j]);
-  //     printf("\n");
-  //   }
+  // make_IntTriple(rand_between(0, n - 2) | 1, rand_between(0, n - 2) | 1, rand_between(0, 3));
+
+  print(maze, n);
+
+  test(maze, n);
+
+  print(maze, n);
+
+  for (unsigned row = 0; row < n; row++) free(maze[row]);
+  free(maze);
 }
