d2c46e5caa68452677270cf53f81a2ec934e4863
wytsai7660 <wytsai7660@gmail.com>
2023-09-21 15:51:01 +0800

    -

--- project_1/solution1.c
+++ project_1/solution1.c
@@ -1,22 +1,37 @@
+// i have no idea what i'm writing
+// this file is a piece of s**t
+// why am i here
+
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// typedef struct {
-//   int x, y, from_dir;
-//   int *ans;
-// } Point;
+// y ^
+//   |             up(0)
+//   |              |
+//   |   left(3) -- + -- right(1)
+//   |              |
+//   |            down(2)
+//   |
+//   + ----------------------> x
+const int direction[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};  // dir_num -> dir[x/y]
 
-typedef struct __Node__ {
-  int x, y, from_dir;
-  int *ans;
-  struct __Node__ *next;
+typedef struct {
+  int x, y, from_dir, *ans, ans_len;
+} Point;
+
+typedef struct Node {
+  Point data;
+  struct Node *next;
 } Node;
 
 typedef struct {
   Node *head, *tail;
   int size;
-} Queue;
+} Queue;  // for this task there is no need for implementing standalone "front" function
 
-Queue *new_queue() {
+Queue *new_queue(void) {
   Queue *tmp_q = malloc(sizeof(Queue));
   *tmp_q = (Queue){NULL, NULL, 0};
   return tmp_q;
@@ -34,7 +49,7 @@ void queue_push(Queue *queue, Point point) {
   queue->size++;
 }
 
-Point queue_pop(Queue *queue) {
+Point queue_pop(Queue *queue) {  // pop out & return the front element
   if (queue->size == 0) {
     printf("queue: attempt to pop an element from an empty queue\n");
     exit(1);
@@ -43,17 +58,21 @@ Point queue_pop(Queue *queue) {
     free(queue->head);
     queue->head = queue->tail = NULL;
   }
-  Point tmp_Point free(queue->head);
-
-  // Point tmp_point = queue->head->data;
-  // Node *tmp_node = queue->head;
-  // queue->head = queue->head->next;
-  // free(tmp_node);
+  Node tmp_node = *queue->head;
+  free(queue->head);
+  queue->head = tmp_node.next;
   queue->size--;
-  // return tmp_point;
+
+  return tmp_node.data;
+}
+
+void queue_free(Queue *queue) {
+  while (queue->size) queue_pop(queue);
+  free(queue);
 }
 
 int main() {
+  // input area starts here
   int n;  // row & col
   scanf("%d", &n);
 
@@ -64,33 +83,42 @@ int main() {
 
   int tmp_1, tmp_2;  // used to temporarily store the input
   scanf("%d%d", &tmp_1, &tmp_2);
-  Point ball_1 = (Point){tmp_1, tmp_2, -1};  // set the ball
+  Point ball_1 = (Point){tmp_1, tmp_2, -1, NULL, 0};  // set the ball
 
   scanf("%d%d", &tmp_1, &tmp_2);
-  Point ball_2 = (Point){tmp_1, tmp_2, -1};  // set the ball
-
-  scanf("%d%d", &tmp_1, &tmp_2);
-  maze[tmp_2][tmp_1] = 'D';  // set the destination
+  Point ball_2 = (Point){tmp_1, tmp_2, -1, NULL, 0};  // set the ball
 
   scanf("%d%d", &tmp_1, &tmp_2);
   maze[tmp_2][tmp_1] = 'D';  // set the destination
 
-  Queue *q = new_queue(n * n);
-  queue_push(q, ball_1);
+  scanf("%d%d", &tmp_1, &tmp_2);
+  maze[tmp_2][tmp_1] = 'D';  // set the destination
 
+  Queue *queue = new_queue();
+  queue_push(queue, ball_1);
+  // input area ends here
+
+  int *ans, ans_len;
   int x, y;
-  while (q->size) {  // BFS for the first ball (two destinations are ok)
-    Point p = queue_pop(q);
-    for (int dir = 0; dir < 4; dir++) {            // try to move in each directionß
-      if (dir != 3 - p.from_dir &&                 // not going back
-          (x = p.x + direction[dir][0]) != '1' &&  //
-          (y = p.y + direction[dir][1]) != '1') {
-        // int x = p.x + direction[dir][0], y = p.y + direction[dir][1];
-        // if (x >= 0 && x < n && y >= 0 && y < n && maze[y][x] != 'X') {
-        //   maze[y][x] = 'X';
-        //   queue_push(q, (Point){x, y, dir});
-        // }
+  bool continue_loop = true;
+  while (continue_loop) {  // BFS for the first ball (two destinations are ok)
+    const Point working_point = queue_pop(queue);
+    for (int dir = 0; dir < 4; dir++) {                 // try to move in each direction
+      if (dir == 4 - working_point.from_dir) continue;  // avoid going back
+      x = working_point.x + direction[dir][0], y = working_point.y + direction[dir][1];
+      if (x < 0 || x >= n || y < 0 || y >= n || maze[y][x] == '1') continue;  // out of range or wall
+
+      if (maze[y][x] == 'D') {
+        continue_loop = false, ans = working_point.ans, ans_len = working_point.ans_len;
+        break;
       }
+      Point tmp_point;
+      tmp_point.x = x, tmp_point.y = y, tmp_point.from_dir = dir;
+      tmp_point.ans_len = working_point.ans_len + 1;
+      tmp_point.ans = memcpy(malloc(tmp_point.ans_len * sizeof(int)), working_point.ans, tmp_point.ans_len * sizeof(int));
+      tmp_point.ans[tmp_point.ans_len - 1] = dir;
+
+      queue_push(queue, tmp_point);
     }
   }
 }
